1c8cb8c7015b5fa7c4087ee04581c217d719abc7
Nimisha Goel <goelnimisha1@gmail.com>
2021-06-17 14:57:22 +0530

    Update debugging.py

--- debugging.py
+++ debugging.py
@@ -4,18 +4,24 @@ import subprocess
 import webbrowser
 import os.path
 from os import path
+import string    
+import random  
+
+S = 8    
+ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = S))    
+alpha= str(ran)
 
 #Load Json
 with open('test1.json', encoding="utf8") as f:
  data = json.load(f)
 
 #Check if text file exists
-if path.exists("demo36.txt"):
+if path.exists("%s.txt"%alpha):
     print("File already exists, create new text file!")
     
 #Else create new text file and write the PlantUML Descriptor
 else:
-    file = open("demo36.txt", "w+")
+    file = open("%s.txt"%alpha, "w+")
     file.write("@startuml\n")
     file.write("skinparam componentstyle uml2\n")
     file.write("left to right direction\n")
@@ -59,13 +65,16 @@ else:
                             file.write("\n")
     file.write("@enduml")
     file.close()
-
-#Execute plantuml.jar to generate diagram
-subprocess.run("java -jar plantuml.jar -tpng demo36.txt",capture_output=True)
-
-#Open the diagram in browser
-if path.exists("home6.html"):
-    print("File already exists, create new HTML file!")
-else:
-    open("home6.html","w+").write('<!DOCTYPE html>\n<html>\n<body>\n<center>\n<h1><b>Debugging Session</b></h1><br>\n<img src="demo36.png">\n<center>\n</html>')
-    webbrowser.open('file:///' + os.path.realpath("home6.html"))
+    
+    #Execute plantuml.jar to generate diagram
+    subprocess.run("java -jar plantuml.jar -tpng %s.txt"%alpha,capture_output=True)
+    
+    #Open the diagram in browser
+    beta= "%s.png"%alpha
+    if path.exists("%s.html"%alpha):
+        print("File already exists, create new HTML file!")
+    else:
+        message = """<html>\n<center>\n<h1><b>Debugging Session</b></h1>\n<body>\n<img src="{URL}">\n</body>\n<center>\n</html>"""
+        new_message = message.format(URL=beta)
+        open("%s.html"%alpha,"w+").write(new_message)
+        webbrowser.open('file:///' + os.path.realpath("%s.html"%alpha))
